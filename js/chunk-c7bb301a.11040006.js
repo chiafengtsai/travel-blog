4e8afb0cf49851777fdfbfc0c6ead3e706bf4baf
Chia <chia@punters.com.au>
2020-02-22 00:26:24 +1100

    chore: run build.

--- js/chunk-c7bb301a.11040006.js
+++ js/chunk-c7bb301a.11040006.js
@@ -1,2 +1,2 @@
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-c7bb301a"],{"0c1e":function(t,n,a){},b350:function(t,n,a){"use strict";var l=a("0c1e"),r=a.n(l);r.a},f304:function(t,n,a){"use strict";a.r(n);var l=function(t,n){var a=n._c;return a("div",[a("div",{staticClass:"gallery-wrap"},n._l(n.props.imgUrls,(function(t){return a("img",{key:t,staticClass:"gallery__img",attrs:{"data-src":t}})})),0)])},r=[],e={name:"Gallery",props:{imgUrls:{type:Array,default:null},text:{type:String,default:null}}},s=e,u=(a("b350"),a("2877")),c=Object(u["a"])(s,l,r,!0,null,null,null);n["default"]=c.exports}}]);
-//# sourceMappingURL=chunk-c7bb301a.24666d3c.js.map
+//# sourceMappingURL=chunk-c7bb301a.11040006.js.map
